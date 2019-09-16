4c70183339ab4867fc1989bb156c2af4a3e0e7ae
x893675 <x893675@163.com>
2019-09-16 15:52:31 +0800

    :construction: study cobra and viper in progress

---
+++ config-parse/cmd/print.go
@@ -0,0 +1,16 @@
+package cmd
+
+import (
+	"config-parse/config"
+
+	"github.com/spf13/cobra"
+)
+
+var printCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Get Versions of Clairctl and underlying services",
+	Long:  `Get Versions of Clairctl and underlying services`,
+	Run: func(cmd *cobra.Command, args []string) {
+		config.Print()
+	},
+}
