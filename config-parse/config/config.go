80650142819176d3c8a4e0f65c7df84d3be768f8
x893675 <x893675@163.com>
2019-09-16 15:43:04 +0800

    :construction: study cobra and viper in progress

---
+++ config-parse/config/config.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/coreos/pkg/capnslog"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+	//"github.com/BurntSushi/toml"
+)
+
+var log = capnslog.NewPackageLogger("config-parse", "config")
+
+// Config 配置参数
+type Config struct {
+	RunMode string `toml:"run_mode"`
+	Log     Log    `toml:"log"`
+}
+
+// Log 日志配置参数
+type Log struct {
+	Level      int    `toml:"level"`
+	Format     string `toml:"format"`
+	Output     string `toml:"output"`
+	OutputFile string `toml:"output_file"`
+}
+
+func Init(cfgFile string) {
+	capnslog.SetGlobalLogLevel(capnslog.WARNING)
+	capnslog.SetFormatter(capnslog.NewPrettyFormatter(os.Stdout, false))
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
+	err := viper.ReadInConfig()
+	if err != nil {
+		e, ok := err.(viper.ConfigParseError)
+		if ok {
+			log.Fatalf("error parsing config file: %v", e)
+		}
+		log.Debugf("No config file used")
+	} else {
+		log.Debugf("Using config file: %v", viper.ConfigFileUsed())
+	}
+}
+
+func values() Config {
+	return Config{
+		RunMode: viper.GetString("run_mode"),
+		Log: Log{
+			Level:      viper.GetInt("level"),
+			Format:     viper.GetString("format"),
+			Output:     viper.GetString("output"),
+			OutputFile: viper.GetString("output_file"),
+		},
+	}
+}
+
+func Print() {
+	cfg := values()
+	cfgBytes, err := yaml.Marshal(cfg)
+	if err != nil {
+		log.Fatalf("marshalling configuration: %v", err)
+	}
+
+	fmt.Println("Configuration")
+	fmt.Printf("%v", string(cfgBytes))
+}
--- config-parse/go.mod
+++ config-parse/go.mod
@@ -1,3 +1,11 @@
 module config-parse
 
 go 1.12
+
+require (
+	github.com/coreos/go-systemd v0.0.0-20190719114852-fd7a80b32e1f // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
+	gopkg.in/yaml.v2 v2.2.2
+)
